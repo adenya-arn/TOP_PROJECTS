524b7d6f1eab74852a4f21e2cb73dd6dd2836431
adenya-arn <aadenya7@gmail.com>
2026-02-17 18:00:05 +0300

    Added prompt for squares one wants, square color is now random when one hoversnot black, opacity commented out needs to be worked on

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -2,25 +2,59 @@ const container = document.querySelector("#container");
 
 const div = document.createElement("div");
 
-const button = document.querySelector("#btn1");
+const button = document.querySelector("#grid");
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid");
+const reset = document.querySelector("#reset");
 
-  div.addEventListener("mouseenter", (evnt) => {
-    evnt.target.style.backgroundColor = "black";
+reset.addEventListener("click", (evnt) => {
+  const squares = document.querySelectorAll(".grid");
+
+  squares.forEach((square) => {
+    square.style.backgroundColor = "";
   });
+});
 
-  container.appendChild(div);
-}
+const getRandomValue = () => {
+  return Math.floor(Math.random() * 256);
+};
 
 button.addEventListener("click", (evnt) => {
-  squares = prompt(
+  const squares = prompt(
     "How many squares do you want for your square grid?(100 max) ",
   );
 
   if (squares > 100)
     return alert("Please use a number less than or equal to 100");
-  console.log(squares);
+
+  container.innerHTML = "";
+  const containerSize = container.clientWidth;
+  const size = containerSize / squares;
+  for (let i = 0; i < squares * squares; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+
+    div.addEventListener("mouseenter", (evnt) => {
+      const square = evnt.target;
+
+      let darkness = Number(square.dataset.darkness);
+
+      // if (darkness < 10) {
+      //   darkness++;
+      //   square.dataset.darkness = darkness;
+
+      const opacity = darkness * 0.1;
+
+      const r = getRandomValue();
+      const g = getRandomValue();
+      const b = getRandomValue();
+
+      square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      square.style.opacity = opacity;
+      //}
+    });
+
+    container.appendChild(div);
+  }
 });
