31877f25bdaa640dd2f97b5abf05ab6329ae51d6
adenya-arn <aadenya7@gmail.com>
2026-03-15 02:02:23 +0300

    Initial commit

---
+++ library/main.js
@@ -0,0 +1,62 @@
+//Library array
+const myLibrary = [{ title: "Arnold", author: "adenya" }];
+
+//Constructor function for a book
+function BookObjectCreator(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.id = crypto.randomUUID();
+  this.read = read;
+  this.info = function () {
+    console.log(
+      `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`,
+    );
+  };
+}
+// //prototype function that everybook should have
+// BookObjectCreator.prototype.info = function () {
+//   const bookInfo = [this.title, this.author, this.pages, this.read];
+//   return bookInfo;
+// };
+
+//function to add book to library and first chyecks if it exists
+function addBookToLibrary(title, author, pages, read) {
+  if (
+    myLibrary.find((item) => item.title.toLowerCase() === title.toLowerCase())
+  ) {
+    alert("Book already exists!!");
+    return;
+  }
+  const book = new BookObjectCreator(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+//tried creatin a book
+// addBookToLibrary("aaaaa", "annette", 20, true);
+console.log(myLibrary);
+
+//DOM manipulation
+const container = document.querySelector(".container");
+const bookCard = document.querySelector(".bookCard");
+const ul = document.createElement("ul");
+bookCard.appendChild(ul);
+
+myLibrary.forEach((element) => {
+  const li = document.createElement("li");
+  console.log(element);
+  li.textContent = element.bookInfo;
+  ul.appendChild(li);
+  console.log(li);
+});
+
+//form to add a new book
+
+const addBook = document.querySelector("#addBook");
+const form = document.createElement("form");
+
+const addBookForm = () => {
+  container.appendChild(form);
+  input.classList.add("Author");
+};
+addBook.addEventListener("click", addBookForm);
