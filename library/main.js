a62491af6b93f4c6dd59b989d4027744684d1578
adenya-arn <aadenya7@gmail.com>
2026-03-16 15:27:16 +0300

    Added book form and a few logic

--- library/main.js
+++ library/main.js
@@ -34,7 +34,7 @@ function addBookToLibrary(title, author, pages, read) {
 
 //tried creatin a book
 // addBookToLibrary("aaaaa", "annette", 20, true);
-console.log(myLibrary);
+// console.log(myLibrary);
 
 //DOM manipulation
 const container = document.querySelector(".container");
@@ -52,11 +52,97 @@ myLibrary.forEach((element) => {
 
 //form to add a new book
 
-const addBook = document.querySelector("#addBook");
+const newBook = document.querySelector("#newBook");
 const form = document.createElement("form");
 
 const addBookForm = () => {
-  container.appendChild(form);
-  input.classList.add("Author");
+  // container.appendChild(form);
+
+  const titleLabel = document.createElement("label");
+  titleLabel.textContent = "Title";
+  titleLabel.for = "title";
+  const titleInput = document.createElement("input");
+  titleInput.id = "title";
+  titleInput.type = "text";
+  titleInput.placeholder = "Book Title";
+
+  const authorLabel = document.createElement("label");
+  authorLabel.textContent = "Author";
+  authorLabel.for = "author";
+  const authorInput = document.createElement("input");
+  authorInput.id = "author";
+  authorInput.type = "text";
+  authorInput.placeholder = "Author";
+
+  const pagesLabel = document.createElement("label");
+  pagesLabel.textContent = "Number of Pages";
+  pagesLabel.for = "pages";
+  const pagesInput = document.createElement("input");
+  pagesInput.id = "pages";
+  pagesInput.type = "numbers";
+  pagesInput.placeholder = "Pages";
+
+  const readLabel = document.createElement("label");
+  readLabel.textContent = "Read";
+  readLabel.for = "read";
+  const readRadio = document.createElement("input");
+  readRadio.id = "read";
+  readRadio.value = "true";
+  readRadio.type = "radio";
+  readRadio.name = "readStatus";
+
+  const notReadLabel = document.createElement("label");
+  notReadLabel.textContent = "Not Read";
+  notReadLabel.for = "notRead";
+  const notReadRadio = document.createElement("input");
+  notReadRadio.id = "notRead";
+  notReadRadio.value = "false";
+  notReadRadio.type = "radio";
+  notReadRadio.name = "readStatus";
+
+  const submitBtn = document.createElement("button");
+  submitBtn.textContent = "Add Book";
+
+  form.append(
+    titleLabel,
+    titleInput,
+    authorLabel,
+    authorInput,
+    pagesLabel,
+    pagesInput,
+    readLabel,
+    readRadio,
+    notReadLabel,
+    notReadRadio,
+    deleteBtn,
+  );
+
+  container.append(form);
+
+  submitBtn.addEventListener("click", (evnt) => {
+    evnt.preventDefault();
+
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const selected = document.querySelector("input[name='readStatus']:checked");
+    const readValue = selected ? selected.value === "true" : false;
+    addBookToLibrary(title, author, pages, readValue);
+  });
 };
-addBook.addEventListener("click", addBookForm);
+
+// form.addEventListener("submit", (evnt) => {
+//   evnt.preventDefault();
+//   const title = titleI
+// })
+newBook.addEventListener("click", addBookForm);
+
+console.log(myLibrary);
+
+// functions to display book, delete book
+
+BookObjectCreator.prototype.deleteBook = function () {
+  const deleteBook = document.createElement("button");
+  deleteBook.classList.add("deleteBook");
+  deleteBook.textContent = "Delete";
+};
